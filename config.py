1d0f9ea5f0ac4598092830fee49909224ddd9a59
VipFitnessBot <19dib79@gmail.com>
2025-08-29 00:05:27 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_TOKEN = "7717901847:AAHytaN_hObl-6G8IB43r8qhRSZ7svnO6gM"
 WFP_MERCHANT = "www_instagram_com_84d14"
 WFP_SECRET = "23434a4fff7cd0e1b1b2f928ffa41d40370bc4b1"
-CALLBACK_URL = "https://your-railway-url.up.railway.app/wfp-callback"
+CALLBACK_URL = "https://vipfitparkovabot-production.up.railway.app/wfp-callback"
