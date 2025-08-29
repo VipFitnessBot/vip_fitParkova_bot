f5ed66d75e916023cd224111549f743f41736d0e
VipFitnessBot <19dib79@gmail.com>
2025-08-29 17:16:48 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 TELEGRAM_TOKEN = "7717901847:AAHytaN_hObl-6G8IB43r8qhRSZ7svnO6gM"
-WFP_MERCHANT = "www_instagram_com_84d14"
-WFP_SECRET = "23434a4fff7cd0e1b1b2f928ffa41d40370bc4b1"
+MERCHANT_ACCOUNT = "www_instagram_com_84d14"
+MERCHANT_SECRET_KEY = "23434a4fff7cd0e1b1b2f928ffa41d40370bc4b1"
 CALLBACK_URL = "https://vipfitparkovabot-production.up.railway.app/wfp-callback"
+MERCHANT_DOMAIN_NAME = "https://www.instagram.com/fitnessclubparkova"
